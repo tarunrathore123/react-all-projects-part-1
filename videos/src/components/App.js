b865ef8c20b274335b33dd3a4c542d70c0a4a2b1
tarunrathore123 <31217259+tarunrathore123@users.noreply.github.com>
2022-09-11 19:07:16 +0530

    added videos

---
+++ videos/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    state={term:"tarun"}
+    render(){
+        return(
+        <SearchBar />
+        );
+    }
+}
+
+export default App;
